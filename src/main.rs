6e5ceccf4c7f031e4e37ad3a8b483ab810a28171
Jeff Trempe <trempe.jeff@gmail.com>
2023-09-16 00:01:16 -0400

    adding basic file manipulation

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,26 @@
 use growers::Grower;
 mod growers;
+use std::fs::File;
+use std::io::Read;
+use std::io::Write;
+
+fn create_db() {
+    let mut data_file = File::create("data.json").expect("creation failed");
+
+    data_file.write("{}".as_bytes()).expect("write failed");
+}
+
+fn remove_db() {
+    fs::remove_file("data.json").expect("could not remove file");
+
+    println!("Removed file data.txt");
+}
+
+fn read_db() {
+    let mut data_file = File::open("data.json").expect("creation failed");
+
+    data_file.read().expect("write failed");
+}
 
 fn main() {
     let grower = growers::MockedPie::new(4, 5);
@@ -7,4 +28,7 @@ fn main() {
     grower.water();
 
     println!("Hello, world!");
+
+    // Create a file
+    create_db()
 }
