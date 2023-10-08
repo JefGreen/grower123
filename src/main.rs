3739298e6cdad2b8488ba68ac1f3612a5edd55b2
Jeff Trempe <trempe.jeff@gmail.com>
2023-10-08 17:24:07 -0400

    adding register for raspberry_pi

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,14 @@ use std::fs::File;
 use std::io::{Read, Write};
 use std::{thread, time};
 
+fn get_register_for_raspberry_pi3_b(pin: u32) -> u32 {
+    let reg: u32 = pin / 10;
+    return match reg {
+        0 => 34,
+        _ => panic!("Something has gone wrong!"),
+    };
+}
+
 struct GPIO;
 
 impl GPIO {
