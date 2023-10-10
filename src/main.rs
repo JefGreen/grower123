31e1b23ef2857e1873d54f6f78c13806336fe0a0
Jeff Trempe <trempe.jeff@gmail.com>
2023-10-09 22:51:58 -0400

    will change for embeded

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,8 @@ use std::fs::File;
 use std::io::{Read, Write};
 use std::{thread, time};
 
+// TODO: change for embeded
+
 fn get_register_for_raspberry_pi3_b(pin: u32) -> u32 {
     let reg: u32 = pin / 10;
     return match reg {
